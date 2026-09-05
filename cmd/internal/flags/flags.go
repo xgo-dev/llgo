23872ef63014c08ffdd00ee4e3934690caa20c23
ZhouGuangyuan <zhouguangyuan.xian@gmail.com>
2026-09-05 23:33:13 +0800

    feat(lto): support pass plugins on macOS

--- cmd/internal/flags/flags.go
+++ cmd/internal/flags/flags.go
@@ -138,7 +138,7 @@ func AddLTOFlag(fs *flag.FlagSet) {
 	LTO = ltoFlag{Mode: lto.Off}
 	LTOPluginPath = ""
 	fs.Var(&LTO, "lto", "Enable LTO optimization: thin or full (default: off)")
-	fs.StringVar(&LTOPluginPath, "lto-pass-plugin", "", "Load an LLVM LTO pass plugin during full LTO (ELF lld only)")
+	fs.StringVar(&LTOPluginPath, "lto-pass-plugin", "", "Load an LLVM LTO pass plugin during full LTO (ELF lld or Mach-O ld64.lld)")
 }
 
 var GoGlobalDCE *bool
